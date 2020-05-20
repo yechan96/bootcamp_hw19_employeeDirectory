beaa3de826bdfa6b198dc54c4d672ad36460ccaf
yechan96 <yechanlee96@gmail.com>
2020-05-19 23:53:36 -0700

    Revert "Updates"
    This reverts commit 59ab9c5917392555b58ab21de9823ec8816628ab.

---
+++ src/components/SearchForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    <form>
+      <div className="form-group">
+        <input
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search for an Employee"
+          id="search"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
