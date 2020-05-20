66987e31c8ab516d97d1d4ff2a398627c4077c8e
yechan96 <yechanlee96@gmail.com>
2020-05-19 23:25:51 -0700

    Updates

---
+++ precache-manifest.3a281afb68acbda2b732b7d0134f95f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071397ea6b1066980091e185ab362bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae744f5b400cb3ea1892",
+    "url": "/static/js/2.2557d3df.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/static/js/2.2557d3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad50837f98bb3730056",
+    "url": "/static/js/main.82aefe51.chunk.js"
+  },
+  {
+    "revision": "eea0dd2db9c33097b302",
+    "url": "/static/js/runtime-main.766f11de.js"
+  }
+]);
